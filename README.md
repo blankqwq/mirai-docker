692e969555662c3c33ad85f3add98459da60fb9b
blankqwq <1136589038@qq.com>
2021-09-04 22:35:59 +0800

    add .env.example

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ### 快速使用你的Mirai
 
+
+
+> cp .evn.example .env
+
 > docker-compose up
--- mcl/run.sh
+++
@@ -1,6 +0,0 @@
-
-
-if [ "${HTTP_ADAPTOR_URL}" != "" ]; then \
-    echo "${IP_GATEWAY} ${HTTP_ADAPTOR_URL}" >> /etc/hosts && \
-    echo "${IP_GATEWAY} ${HTTP_ADAPTOR_URL}" \
-;fi
